8d7f788744dd805032c2179a1a841ebf374e3e42
Dale Tuttle <tuttld@rpi.edu>
2024-03-04 18:58:01 -0500

    Update nvrAlerts.py

--- src/nvrAlerts.py
+++ src/nvrAlerts.py
@@ -23,7 +23,8 @@ class broker:
 
 
     def __init__(self):
-        self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        #self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.mqttc = mqtt.Client()
 
         #self.debug = config["debug"]["mqttlistener"]
         self.mqttc.on_message = self.on_message
@@ -35,7 +36,7 @@ class broker:
         self.scoreLookup = {}
         self.isMoving = {}
         self.runcount = 0
-        self.motionAlert = {}
+        self.motionAlert = False
         self.motionActive = datetime.datetime.now() - datetime.timedelta(minutes=1)
 
 
@@ -44,38 +45,30 @@ class broker:
 
     def manageAlert(self):
         removeList = []
-        
-        for thing in removeList:
-            del self.motionAlert[thing]
-        removeList = []
-        for thing in self.isMoving:
-            if self.isMoving[thing] < (datetime.datetime.now() - datetime.timedelta(minutes=2)):
-                print(f'{thing} has not moved, removing')
-                removeList.append(thing)
-        for thing in removeList:
-            del self.isMoving[thing]
-
-        removeList = []
         for obj in self.idLookup:
+            
             if "lastSeenMotion" in self.idLookup[obj]:
-                #if no motion for 2 mins
-                #if self.idLookup[obj]["lastSeenMotion"] < (datetime.datetime.now() - datetime.timedelta(minutes=2)):
-                    # if no history updates in 30 mins
-                if self.idLookup[obj]["lastSeen"] < (datetime.datetime.now() - datetime.timedelta(minutes=30)):
-                    print(f'{obj} can be removed, no motion or history in 30 mins')
-                    removeList.append(obj)
-                elif self.idLookup[obj]["lastSeenMotion"] > (datetime.datetime.now() - datetime.timedelta(minutes=1)):
-                    self.motionActive = self.idLookup[obj]["lastSeenMotion"]
-        
+                if self.motionActive < self.idLookup[obj]["lastSeenMotion"]:
+                    print(f'update motion active because of {obj}, {self.idLookup[obj]["className"]} {self.idLookup[obj]["zones"]}')
+                    self.motionActive =self.idLookup[obj]["lastSeenMotion"]
+                
+             # if no history updates in 30 mins, forget object
+            if self.idLookup[obj]["lastSeen"] < (datetime.datetime.now() - datetime.timedelta(minutes=30)):
+                removeList.append(obj)
+
+        #clean up
         for thing in removeList:
+            print(f'remove {thing}')
             del self.idLookup[thing]
 
-            #else:
-            #    print(f'{self.idLookup[obj]} no lastseen movement to check')
-
-                
         if self.motionActive > (datetime.datetime.now() - datetime.timedelta(minutes=1)):
-            print('motion')
+            if self.motionAlert == False:
+                print(f'found motion {datetime.datetime.now()}')
+                self.motionAlert = True
+        else:
+            if self.motionAlert == True:
+                print(f'motion clear {datetime.datetime.now()}')
+                self.motionAlert = False
 
 
 
@@ -89,67 +82,59 @@ class broker:
         if isinstance(msgjson, dict):
             if len(msgjson["detections"]) > 0:
                 for trackedObject in msgjson["detections"]:
-                    firstDetect = False
                     # keep log of how often we look at objects
-                    self.runcount +=1
+                    objdict = {}
                     #check for required fields
                     if True:
                         if "id" not in trackedObject and "className" in trackedObject:
                             if trackedObject["className"] != "motion":
                                 print('pass on no id, its not motion')
                                 print(trackedObject)
-                                continue
-                            else:
-                                print('pass on no id')
-                                print(trackedObject)
-                                continue
-                        
+                            continue
+
                         if "score" in trackedObject and "className" in trackedObject and "id" in trackedObject and "zones" in trackedObject:
-                            objdict = {}
-                            id = trackedObject['id']
+                            
                             # tracking with id and firstseen
                             if trackedObject["id"] in self.idLookup:
                                 objdict = self.idLookup[trackedObject["id"]]
                                 objdict["count"] = objdict["count"] + 1
+                                objdict["lastSeen"] = datetime.datetime.fromtimestamp(trackedObject["history"]["lastSeen"]/1000)
                             else:
-                                print(f'found new {trackedObject["className"]} with id {trackedObject["id"]}. zones {trackedObject["zones"]}')
+                                print(f'found new {trackedObject["className"]} with id {trackedObject["id"]}. zones {trackedObject["zones"]}, box {trackedObject["boundingBox"]}. Its - {datetime.datetime.now()}')
                                 objdict["count"] = 1
                                 objdict["className"] = trackedObject["className"]
                                 objdict["firstSeen"] = datetime.datetime.fromtimestamp(trackedObject["history"]["firstSeen"]/1000)
                                 objdict["lastSeen"] = datetime.datetime.fromtimestamp(trackedObject["history"]["lastSeen"]/1000)
                                 objdict["zones"] = trackedObject["zones"]
+                                if len(trackedObject["movement"])> 0:
+                                    print(f'new {trackedObject["className"]} with id {trackedObject["id"]} has movement')
+                                if "Property" in trackedObject["zones"]:
+                                    print(f'new {trackedObject["className"]} with id {trackedObject["id"]} is on property')
 
-                                firstDetect = True  
                             # update zones
-
                             if trackedObject["zones"] != objdict["zones"]:
+                                # see if object moved from all to property
+                                if "Property" in trackedObject["zones"] and "Property" not in objdict["zones"]:
+                                    if len(trackedObject["movement"])> 0:
+                                        print(f'{objdict["className"]}, {trackedObject["id"]} moved from all to property because of movement')
+                                    else:
+                                        print(f'{objdict["className"]}, {trackedObject["id"]} moved from all to property.s')
+                                    print(trackedObject)
+                                else:
+                                    print(f'different zone change for {objdict["className"]} was {objdict["zones"]} is {trackedObject["zones"]}')
+                                # reset object zones
                                 objdict["zones"] = trackedObject["zones"]
+
                             # check for movement
+                            # if there is movement update lastSeen
                             if len(trackedObject["movement"])> 0:
+                                objdict["lastSeenMotion"] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
                                 objdict["movement"] = trackedObject["movement"]
+                                
                             
                             #save objdict
                             self.idLookup[trackedObject["id"]] = objdict
-
-                               
-
-                            if "history" in trackedObject:
-                                if trackedObject["history"]["firstSeen"] in self.firstseenLoopup:
-                                    self.firstseenLoopup[trackedObject["history"]["firstSeen"]] = self.firstseenLoopup[trackedObject["history"]["firstSeen"]] +1
-                                else:
-                                    self.firstseenLoopup[trackedObject["history"]["firstSeen"]] = 1 
-                            else:
-                                print('ERROR - no history')     
-                                print(trackedObject)
-
-
-                            # if there is movement update lastSeen
-                            if len(trackedObject["movement"]) > 0:              
-                                self.idLookup[trackedObject["id"]]["lastSeenMotion"] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-                                self.isMoving[trackedObject['id']] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-
-                            
-
+    
                             # property should be for someone coming home
                             if "Property" in objdict["zones"]:
  
@@ -157,49 +142,11 @@ class broker:
 
                                     # this is a new object, so someone pulling into driveway?
                                     if historyFirstSeen > (datetime.datetime.now() - datetime.timedelta(minutes=5)):
-                                        self.motionAlert[trackedObject["className"]] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
                                         print(f"property {trackedObject['className']} {trackedObject['id']}, with movement. first seen is greater than now - 5 mins {trackedObject['id']}. Was first seen {historyFirstSeen}")
+                                    else:
+                                        print(f"property {trackedObject['className']} {trackedObject['id']}, with movement. first seen is less than now - 5 mins {trackedObject['id']}. Was first seen {historyFirstSeen}")
                                    
-                        
-
-    def old(self):
-
-    # if there is data in movement it moved
             
-        if len(trackedObject["movement"])> 0:
-            self.idLookup[trackedObject["id"]]["lastSeen"] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-
-            if trackedObject["className"] not in self.motionAlert:
-                print('motion')
-            #update last moved
-            self.isMoving[trackedObject['id']] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-
-            #track last motion for object type
-            self.motionAlert[trackedObject["className"]] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-            
-            #if "lastSeen" in trackedObject["movement"] and "firstSeen" in trackedObject["movement"]:
-            firstSeen = datetime.datetime.fromtimestamp(trackedObject["movement"]["firstSeen"]/1000)
-            lastSeen = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-            
-            self.idLookup[trackedObject["id"]]["lastSeen"] = datetime.datetime.fromtimestamp(trackedObject["movement"]["lastSeen"]/1000)
-            
-                
-
-            # print(f'{trackedObject["id"]} - {trackedObject["className"]} has movement, firstseen - {firstSeen}, now - {datetime.datetime.now()}')
-                #if self.isMoving[trackedObject['id']] < (datetime.timedelta(seconds=5) - datetime.datetime.now()):
-                #    print(f'{trackedObject["id"]} - {trackedObject["className"]} has movement, firstseen - {firstSeen}, now - {datetime.datetime.now()}')
-
-
-            #else:
-
-                
-                #historyFirstSeen = datetime.datetime.fromtimestamp(trackedObject["history"]["firstSeen"]/1000)
-                # no movement, check firstseen
-                #if historyFirstSeen > (datetime.timedelta(minutes=5) - datetime.datetime.now()):
-                #    print(f"first seen is greater than now - 5 mins {trackedObject['id']} has no movement. Was first seen {historyFirstSeen}")
-                #else:
-                #    print(f"first seen is not greater than now - 5 mins {trackedObject['id']} has no movement. Was first seen {historyFirstSeen}")
-
 
 #print(msgjson["detections"])
     def mqttrunner(self):
@@ -217,7 +164,8 @@ class broker:
         while True:
             if runCount%120 == 0:
                 for item in self.idLookup:
-                    print(item, self.idLookup[item]["count"])
+                    pass
+                    #print(item, self.idLookup[item]["count"])
                 #print(self.idLookup)
             self.manageAlert()
             time.sleep(1)
